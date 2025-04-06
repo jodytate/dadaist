b87821a131c31cc198c613002e804407ff0bffdf
jody tate <jody@josephtate.com>
2025-04-06 14:25:44 -0700

    commit last working version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const moment = require('moment');
+const os = require('os');
+const prependFile = require('prepend-file');
 const shuffle = require('./libs/shuffle.js')
 const sw = require('stopword');
 
@@ -14,17 +16,29 @@ const dada = function dada (array) {
   return output.join(' ');
 }
 
-const input = fs.readFileSync(__dirname + '/input.txt', 'utf8').toString();
+const input = fs.readFileSync(__dirname + '/test.txt', 'utf8').toString();
 const noLineBreaks = input.replace(/(\r\n|\n|\r)/gm, '');
 const splitUp = noLineBreaks.split(' ');
 const stopFiltered = sw.removeStopwords(splitUp);
 const shuffled = shuffle(stopFiltered);
 const newDada = dada(shuffled);
 
-fs.writeFile(`outputs/output-${fileTimestamp}.txt`, newDada, (error) => {
+const outputFile = `outputs/output-${fileTimestamp}.txt`
+
+fs.writeFile(outputFile, newDada, encoding='utf8', (error) => {
   if (error) {
     console.error(error);
   } else {
     console.log('file saved');
   }
-}); 
+});
+
+const prependText = `${humanTimestamp} ${os.EOL} ${os.EOL}`;
+
+prependFile(outputFile, prependText, encoding='utf8', function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('file prepended');
+  }
+});
