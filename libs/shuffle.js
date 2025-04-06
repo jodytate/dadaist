0b8455af73a8db24cf3f9d51d87653d3888bd27c
jody tate <jody@josephtate.com>
2025-04-06 15:08:49 -0700

    new shuffle
    from https://stackoverflow.com/a/12646864

--- libs/shuffle.js
+++ libs/shuffle.js
@@ -1,11 +1,7 @@
-function shuffle (array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
